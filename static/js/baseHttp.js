db00ef6d0d008f41187b460258ad969358e511b4
chenyao <chenyao_6461@163.com>
2018-04-09 09:06:49 +0800

    费

--- static/js/baseHttp.js
+++ static/js/baseHttp.js
@@ -1,10 +1,11 @@
 window.common={
-    predictDomain:"https://app.pre.dingdingkuaixiu.com",  //预发布环境获取支付参数 domain
+    predictDomain:"http://app.pre.dingdingkuaixiu.com",  //预发布环境获取支付参数 domain
     getRequest:"",
     appid:"wxb5b451d85729641a",  //测试环境
+    apidomain:"http://app.dingdingkuaixiu.com",
     apidomain:"http://app.test.dingdingkuaixiu.com",
-   customService :"https://177dd.udesk.cn/im_client/?web_plugin_id=38935",
-   pathDomain:"http://wechat.testdist.dingdingkuaixiu.com",      //支付中转域名  发布正式环境需要修改成正式环境域名
+    customService :"https://177dd.udesk.cn/im_client/?web_plugin_id=38935",
+    pathDomain:"http://wechat.testdist.dingdingkuaixiu.com",      //支付中转域名  发布正式环境需要修改成正式环境域名
     // apidomain:"http://192.168.20.86:8891"  //雍挺孝
     // apidomain:"http://192.168.20.106:8891"  //建伟
     // apidomain:"http://192.168.20.172:891"  //刘伟
@@ -18,9 +19,9 @@ window.common={
 //     pathDomain:"https://wechat.testdist.dingdingkuaixiu.com",      //支付中转域名  发布正式环境需要修改成正式环境域名
 // }
 // window.common={
-//     predictDomain:"https://app.pre.dingdingkuaixiu.com",           //正式环境获取支付参数 domain
+//     predictDomain:"http://app.pre.dingdingkuaixiu.com",           //正式环境获取支付参数 domain
 //     getRequest:"",
-// customService :"https://177dd.udesk.cn/im_client/?web_plugin_id=38935",
+//     customService :"https://177dd.udesk.cn/im_client/?web_plugin_id=38935",
 //     appid:"wxaea6961118bf712d",  //正式环境
 //     apidomain:"https://app.dingdingkuaixiu.com",
 //     pathDomain:"https://wechat.dingdingkuaixiu.com",      //支付中转域名  发布正式环境需要修改成正式环境域名
